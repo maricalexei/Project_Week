e2fee439fe0bb6905e6c0ac26bfd7048834064ac
Maric-Alexei Brouwer <maricalexei@gmail.com>
2021-02-03 14:54:17 +0100

    Change: Added a background image, Type: addition, Why: looks nice

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="shortcut icon" href="./img/icons/favicon.ico" type="image/x-icon">
 
-    <title>Hello, world!</title>
+    <title>CS:GO Wiki</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
