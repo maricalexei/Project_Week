059aca8405effb638779099c2eb2775079aff457
DamienF <329877@student.mboutrecht.nl>
2021-02-04 14:38:03 +0100

    Change: Added gamemode pages and weapons pages Type: Addition Why: it is need because without you cant call it a wiki

---
+++ Gamemodes/wingman.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="shortcut icon" href="../img/icons/favicon.ico" type="image/x-icon">
+
+    <title>CS:GO Wiki | Wingman</title>
+  </head>
+  <body>
+      <!-- This is the Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000;">
+  <div class="container-fluid">
+      <img src="../img/icons/csgoicon.png" id="csicon" alt="Icon">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Weapons
+          </a>
+          <!-- This is the dropdown for the weapons -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../Weapons/knifes.php">Knifes</a></li>
+            <li><a class="dropdown-item" href="../Weapons/pistols.php">Pistols</a></li>
+            <li><a class="dropdown-item" href="../Weapons/smgs.php">Submachine guns</a></li>
+            <li><a class="dropdown-item" href="../Weapons/rifles.php">Rifles</a></li>
+            <li><a class="dropdown-item" href="../Weapons/heavy.php">Heavy</a></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Equipment
+          </a>
+                    <!-- This is the dropdown for the Equipment -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../Equipment/grenades.php">Grenades</a></li>
+            <li><a class="dropdown-item" href="../Equipment/gear.php">Gear</a></li>
+          </ul>
+        <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Gamemodes
+          </a>
+                    <!-- This is the dropdown for the Gamemodes -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="bom.php">Bom defuse</a></li>
+            <li><a class="dropdown-item" href="deathmatch.php">Deathmatch</a></li>
+            <li><a class="dropdown-item" href="hostage.php">Hostage Situation</a></li>
+            <li><a class="dropdown-item" href="wingman.php">Wingman</a></li>
+          </ul>
+          <li class="nav-item dropdown">
+          <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Maps
+          </a>
+                    <!-- This is the dropdown for the maps -->
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="../Maps/dust.php">Dust-2</a></li>
+            <li><a class="dropdown-item" href="../Maps/mirage.php">Mirage</a></li>
+            <li><a class="dropdown-item" href="../Maps/inferno.php">Inferno</a></li>
+            <li><a class="dropdown-item" href="../Maps/duke.php">Duke</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- this is the card for all the information -->
+<div class="container">
+  <div class="row justify-content-md-center">
+    <div class="col-9" id="card1">
+    <div class="card text-white bg-dark mb-3">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
+    </div>
+    <!-- This is the card for the content -->
+    <div class="col-3" id="card1">
+    <div class="card text-white bg-dark mb-3">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
+    </div>
+   </div>
+  </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
